037c69d1e018b6401d879e2567341f9dfacacf81
hemanthlavu <hemanthkumar.lavu@contus.in>
2023-07-28 22:28:49 +0530

    State news update

--- src/app/components/StateNews.js
+++ src/app/components/StateNews.js
@@ -8,12 +8,51 @@ import newsData from '../../assets/data';
 function StateNews() {
   const [data, setData] = useState();
   const { category } = useParams();
+  const [myCategory, setMyCategory] = useState({});
+  const [current, setCurrent] = useState(1);
+  const [totalRecords, setTotalRecords] = useState(0)
+  const [totalPages, setTotalPages] = useState(0);
   
   useEffect(() => {
     if (category) {
-      setData(newsData[category]);
+      fetch("http://localhost:9093/api/v1/category/" + category)
+    .then(res => res.json())
+    .then(data => {
+      if (data.statusCode === 200) {
+        const catData = data?.response?.data;
+        setMyCategory(catData);
+        if (catData._id) {
+          callNewsApi(1, catData._id);
+        }
+      } else {
+        alert('No Cateory found. Try After some time')
+      }
+    })
+    .catch(error => {
+      alert('Error fetching category data');
+    })
     }
-  }, [category]);
+  }, []);
+
+  const callNewsApi = async (page, categoryId = myCategory._id, ) => {
+    await fetch(`http://localhost:9093/api/v1/news/list/${categoryId}?limit=10&page=${page}&sortBy=ASC`)
+    .then(res => res.json())
+    .then(data => {
+      if (data.statusCode === 200) {
+        const response = data?.response;
+        const newsItems = data?.response?.data;
+        setData([...data, ...newsItems]);
+        setCurrent(response?.currentPage);
+        setTotalPages(response?.totalPages);
+        setTotalRecords(response?.total);
+      } else {
+        alert('No Cateory found. Try After some time')
+      }
+    })
+    .catch(error => {
+      alert('Error fetching category data');
+    })
+  }
   return (
       <Container fluid>
           <HomeContent categoryNews={data}/>
